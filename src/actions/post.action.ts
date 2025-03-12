41a35836ab7f16bc496f7c65d7e886bfcdc7837b
Alejandro Apodaca Cordova <apoapps@Alejandros-Mac-mini.local>
2025-03-12 13:32:01 -0700

    update

---
+++ src/actions/post.action.ts
@@ -0,0 +1,9 @@
+"use server";
+
+export async function createPost(content: string, imageUrl: string) {
+  try {
+    
+  } catch (error) {
+    
+  }
+}
---
+++ src/actions/user.action.ts
@@ -0,0 +1,64 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { auth, currentUser } from "@clerk/nextjs/server";
+
+export async function syncUser() {
+  try {
+    const { userId } = await auth();
+    const user = await currentUser();
+
+    if (!userId || !user) return;
+    //check if user exists in db
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        clerkId: userId,
+      },
+    });
+
+    const dbUser = await prisma.user.create({
+      data: {
+        clerkId: userId,
+        name: `${user.firstName} || ""} ${user.lastName || ""}`,
+        username:
+          user.username ?? user.emailAddresses[0].emailAddress.split("@")[0],
+        email: user.emailAddresses[0].emailAddress,
+        image: user.imageUrl,
+      },
+    });
+    return dbUser;
+  } catch (error) {
+    console.log("Error in syncUser", error);
+  }
+}
+
+export async function getUserByClerkId(clerkId: string) {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        clerkId,
+      },
+      include: {
+        _count: {
+          select: {
+            followers: true,
+            following: true,
+          },
+        },
+      },
+    });
+    return user;
+  } catch (error) {
+    console.log("Error in getUserByClerkId", error);
+  }
+}
+
+export async function getDbUserId() {
+  const { userId: clerkId } = await auth();
+  if (!clerkId) throw new Error("Unauthorized");
+  const user = await getUserByClerkId(clerkId);
+
+  if (!user) throw new Error("User not found");
+  
+  return user.id;
+}
